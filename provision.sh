3c4536af31027ccf29175b03bdf6d0fbb39f725a
Denes Stolte <contact@stolte.me>
2024-03-25 21:14:34 +0100

    Added roles for homebrew and mas

---
+++ provision.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+EXTRA_TAGS=""
+
+for tag in "$@"; do
+    EXTRA_TAGS+="$tag "
+done
+
+if [ -n "$EXTRA_TAGS" ]; then
+    ansible-playbook main.yml --ask-become-pass --tags "$EXTRA_TAGS"
+else
+    ansible-playbook main.yml --ask-become-pass
+fi
